d9466b135a1c8f7d7a3c89551dbf9c52b3836e79
Danh Nguyen <danhnguyen267@gmail.com>
2020-04-05 19:09:55 -0700

    Some refactoring

--- utils/auth.js
+++ utils/auth.js
@@ -33,10 +33,7 @@ const checkAuth = (context) => {
     throw new Error('Authorization header must be provided');
 };
 
-const emailRegex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
-
 module.exports = {
     generateToken,
-    checkAuth,
-    emailRegex
+    checkAuth
 };
--- utils/validators.js
+++ utils/validators.js
@@ -1,4 +1,4 @@
-const { emailRegex } = require('./auth');
+const emailRegex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 
 const validateRegisterInput = (
     username,
